9c5f37a446c51b533c621e642197fac382985a1d
zhanw <258653667@qq.com>
2015-08-03 16:16:54 +0800

    update

--- ChatDemoUI/src/com/easemob/applib/widget/chatrow/EMChatRow.java
+++ ChatDemoUI/src/com/easemob/applib/widget/chatrow/EMChatRow.java
@@ -1,5 +1,6 @@
 package com.easemob.applib.widget.chatrow;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.widget.BaseAdapter;
@@ -12,6 +13,29 @@ import com.easemob.chat.EMMessage;
 import com.easemob.chatuidemo.R;
 
 public abstract class EMChatRow extends LinearLayout {
+    protected static final int REQUEST_CODE_EMPTY_HISTORY = 2;
+    protected static final int REQUEST_CODE_CONTEXT_MENU = 3;
+    protected static final int REQUEST_CODE_MAP = 4;
+    protected static final int REQUEST_CODE_TEXT = 5;
+    protected static final int REQUEST_CODE_VOICE = 6;
+    protected static final int REQUEST_CODE_PICTURE = 7;
+    protected static final int REQUEST_CODE_LOCATION = 8;
+    protected static final int REQUEST_CODE_NET_DISK = 9;
+    protected static final int REQUEST_CODE_FILE = 10;
+    protected static final int REQUEST_CODE_COPY_AND_PASTE = 11;
+    protected static final int REQUEST_CODE_PICK_VIDEO = 12;
+    protected static final int REQUEST_CODE_DOWNLOAD_VIDEO = 13;
+    protected static final int REQUEST_CODE_VIDEO = 14;
+    protected static final int REQUEST_CODE_DOWNLOAD_VOICE = 15;
+    protected static final int REQUEST_CODE_SELECT_USER_CARD = 16;
+    protected static final int REQUEST_CODE_SEND_USER_CARD = 17;
+    protected static final int REQUEST_CODE_CAMERA = 18;
+    protected static final int REQUEST_CODE_LOCAL = 19;
+    protected static final int REQUEST_CODE_CLICK_DESTORY_IMG = 20;
+    protected static final int REQUEST_CODE_GROUP_DETAIL = 21;
+    protected static final int REQUEST_CODE_SELECT_VIDEO = 23;
+    protected static final int REQUEST_CODE_SELECT_FILE = 24;
+    protected static final int REQUEST_CODE_ADD_TO_BLACKLIST = 25;
 
 	protected LayoutInflater inflater;
 	protected Context context;
@@ -24,10 +48,12 @@ public abstract class EMChatRow extends LinearLayout {
 	protected RelativeLayout bubbleLayout;
 	protected TextView usernickView;
 //	protected ChatRowViewHolder viewHolder;
+    protected Activity activity;
 
 	public EMChatRow(Context context, EMMessage message, int position, BaseAdapter adapter) {
 		super(context);
 		this.context = context;
+		this.activity = (Activity) context;
 		this.message = message;
 		this.position = position;
 		this.adapter = adapter;
@@ -43,16 +69,9 @@ public abstract class EMChatRow extends LinearLayout {
 		bubbleLayout = (RelativeLayout) findViewById(R.id.rl_bubble);
 		usernickView = (TextView) findViewById(R.id.tv_userid);
 		findViewById();
-		
+		//TODO: 设置用户昵称头像，bubble背景灯
+		setUpView();
 	}
-	
-	/**
-	 * 设置属性等
-	 */
-	public void setUpView() {
-		
-	}
-
 	
 	
 
@@ -62,7 +81,15 @@ public abstract class EMChatRow extends LinearLayout {
 	 */
 	protected abstract void inflatView();
 	
+	/**
+	 * 查找chatrow里的控件
+	 */
 	protected abstract void findViewById();
+	
+	/**
+	 * 设置控件属性等
+	 */
+	protected abstract void setUpView();
 	
 //	/**
 //	 * 设置ViewHolder
--- ChatDemoUI/src/com/easemob/applib/widget/chatrow/EMChatRowText1.java
+++ ChatDemoUI/src/com/easemob/applib/widget/chatrow/EMChatRowText1.java
@@ -1,14 +1,33 @@
 package com.easemob.applib.widget.chatrow;
 
 import android.content.Context;
+import android.content.Intent;
+import android.text.Spannable;
+import android.view.View;
+import android.view.View.OnLongClickListener;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.TextView.BufferType;
 
+import com.easemob.EMCallBack;
+import com.easemob.EMError;
+import com.easemob.chat.EMChatManager;
 import com.easemob.chat.EMMessage;
+import com.easemob.chat.TextMessageBody;
 import com.easemob.chatuidemo.R;
+import com.easemob.chatuidemo.activity.ContextMenu;
+import com.easemob.chatuidemo.utils.SmileUtils;
 
 public class EMChatRowText1 extends EMChatRow{
 
-	public EMChatRowText1(Context context, EMMessage message, int position, BaseAdapter adapter) {
+	private TextView contentView;
+    private ProgressBar progressBar;
+    private ImageView statusView;
+
+    public EMChatRowText1(Context context, EMMessage message, int position, BaseAdapter adapter) {
 		super(context, message, position, adapter);
 	}
 
@@ -20,7 +39,89 @@ public class EMChatRowText1 extends EMChatRow{
 
 	@Override
 	protected void findViewById() {
-		
+		contentView = (TextView) findViewById(R.id.tv_chatcontent);
+		progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        statusView = (ImageView) findViewById(R.id.msg_status);
 	}
+
+    @Override
+    protected void setUpView() {
+        TextMessageBody txtBody = (TextMessageBody) message.getBody();
+        Spannable span = SmileUtils.getSmiledText(context, txtBody.getMessage());
+        // 设置内容
+        contentView.setText(span, BufferType.SPANNABLE);
+        // 设置长按事件监听
+        bubbleLayout.setOnLongClickListener(new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                activity.startActivityForResult(
+                        (new Intent(context, ContextMenu.class)).putExtra("position", position).putExtra("type",
+                                EMMessage.Type.TXT.ordinal()), REQUEST_CODE_CONTEXT_MENU);
+                return true;
+            }
+        });
+
+        if (message.direct == EMMessage.Direct.SEND) {
+            switch (message.status) {
+            case CREATE: 
+                progressBar.setVisibility(View.VISIBLE);
+                statusView.setVisibility(View.GONE);
+                // 发送消息
+                sendMsgInBackground(message);
+                break;
+            case SUCCESS: // 发送成功
+                progressBar.setVisibility(View.GONE);
+                statusView.setVisibility(View.GONE);
+                break;
+            case FAIL: // 发送失败
+                progressBar.setVisibility(View.GONE);
+                statusView.setVisibility(View.VISIBLE);
+                break;
+            case INPROGRESS: // 发送中
+                progressBar.setVisibility(View.VISIBLE);
+                statusView.setVisibility(View.GONE);
+                break;
+            default:
+               break;
+            }
+        }
+    }
+
+    private void sendMsgInBackground(final EMMessage message) {
+        EMChatManager.getInstance().sendMessage(message, new EMCallBack() {
+            
+            @Override
+            public void onSuccess() {
+                updateView(message);
+            }
+            
+            @Override
+            public void onProgress(int progress, String status) {
+                
+            }
+            
+            @Override
+            public void onError(int code, String error) {
+                updateView(message);
+            }
+        });
+    }
+    
+    protected void updateView(EMMessage message){
+        if (message.status == EMMessage.Status.FAIL) {
+            
+            if(message.getError() == EMError.MESSAGE_SEND_INVALID_CONTENT){
+                Toast.makeText(activity, activity.getString(R.string.send_fail) + activity.getString(R.string.error_send_invalid_content), 0).show();
+            }else if(message.getError() == EMError.MESSAGE_SEND_NOT_IN_THE_GROUP){
+                Toast.makeText(activity, activity.getString(R.string.send_fail) + activity.getString(R.string.error_send_not_in_the_group), 0).show();
+            }else{
+                Toast.makeText(activity, activity.getString(R.string.send_fail) + activity.getString(R.string.connect_failuer_toast), 0).show();
+            }
+        }
+
+        adapter.notifyDataSetChanged();
+    }
+    
+    
 
 }
